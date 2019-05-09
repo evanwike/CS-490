ec085867b8da86c3796c0353eddf38d4f7584e16
Evan W <sportsdude716@gmail.com>
2019-05-09 15:10:22 -0500

    Added start and end time selectors
    Added start and end time selectors and an 'all-day event' checkbox. When the checkbox is selected, time selectors will be removed from view.

--- Mobile/7/CalendarApp/app/src/main/java/com/example/calendarapp/CreateEventActivity.java
+++ Mobile/7/CalendarApp/app/src/main/java/com/example/calendarapp/CreateEventActivity.java
@@ -1,31 +1,56 @@
 package com.example.calendarapp;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.provider.CalendarContract;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity {
     EditText editTitle, editDesc, editLocation;
-    Calendar date;
+    CheckBox chkAllDay;
+    LinearLayout createLayout, timeSelectBtns;
     Intent intent;
+    Calendar date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        date = Calendar.getInstance();
         intent = getIntent();
+        date = Calendar.getInstance();
 
         editTitle = findViewById(R.id.eventTitle);
         editDesc = findViewById(R.id.eventDesc);
         editLocation = findViewById(R.id.eventLocation);
+        chkAllDay = findViewById(R.id.chkAllDay);
+        createLayout = findViewById(R.id.createLayout);         // Parent view
+        timeSelectBtns = findViewById(R.id.timeSelectBtns);     // Layout containing time select buttons
 
         date.setTimeInMillis(intent.getLongExtra("date", 0));
+
+        // Remove time select buttons when "All Day Event" checkbox is checked
+        chkAllDay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    createLayout.removeView(timeSelectBtns);
+                else
+                    createLayout.addView(timeSelectBtns, 3);
+            }
+        });
     }
 
     // Go back to MainActivity
@@ -47,5 +72,30 @@ public class CreateEventActivity extends AppCompatActivity {
 
         startActivity(intent);
         finish();
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // TODO: Do something with selected time
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
